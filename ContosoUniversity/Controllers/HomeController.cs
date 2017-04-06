eb9f8ff7b5518f9ab721b2b3328e6a14af05e808
Jason Williams <escapeimagery@live.co.uk>
2017-04-06 22:43:31 +0100

    sorting, filtering, paging. About page and home controller about updated to output student doby statistics

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,9 @@ namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +20,15 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDataGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dataGroup
+                                                   select new EnrollmentDataGroup()
+                                                   {
+                                                       EnrollmentDate = dataGroup.Key,
+                                                       StudentCount = dataGroup.Count()
+                                                   };
 
-            return View();
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +36,12 @@ namespace ContosoUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
